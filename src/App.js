f962d51b93218d113569252ac3d2ee4aff2b7698
Samruddhi16 <gorghatesamruddhi@gmail.com>
2024-01-21 22:44:52 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DrumControl from "./DrumControl";
+import Keyboard from "./Keyboard";
+import { soundsGroup, soundsName } from "./utils";
+const App = () => {
+  const [power, setPower] = React.useState(true);
+  const [volume, setVolume] = React.useState(1);
+  const [soundName, setSoundName] = React.useState("");
+  const [soundType, setSoundType] = React.useState("heaterKit");
+  const [sounds, setSounds] = React.useState(soundsGroup[soundType]);
 
-function App() {
+  const styleActiveKey = (key) => {
+    key.parentElement.style.backgroundColor = "#000000";
+    key.parentElement.style.color = "#ffffff";
+  };
+
+  const deActivatedKey = (audio) => {
+    audio.parentElement.style.backgroundColor = "#ffffff";
+    audio.parentElement.style.color = "#000000";
+  };
+
+  const deactivateAudio = (audio) => {
+    setTimeout(() => {
+      audio.parentElement.style.backgroundColor = "#ffffff";
+      audio.parentElement.style.color = "#000000";
+    }, 300);
+  };
+
+  const play = (key, sound) => {
+    setSoundName(sound);
+    const audio = document.getElementById(key);
+    styleActiveKey(audio);
+    audio.currentTime = 0;
+    audio.play();
+    deactivateAudio(audio);
+  };
+
+  const stop = () => {
+    setPower(!power);
+  };
+
+  const changeSoundGroup = () => {
+    setSoundName("");
+    if (soundType === "heaterKit") {
+      setSoundType("smoothPianoKit");
+      setSounds(soundsGroup.smoothPianoKit);
+    } else {
+      setSoundType("heaterKit");
+      setSounds(soundsGroup.heaterKit);
+    }
+  };
+
+  const handleVolumeChange = (e) => {
+    setVolume(e.target.value);
+  };
+
+  const setKeyVolume = () => {
+    const audioes = sounds.map((sound) => document.getElementById(sound.key));
+    audioes.forEach((audio) => {
+      if (audio) {
+        audio.volume = volume;
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      {setKeyVolume()}
+      <div className="wrapper">
+        <Keyboard
+          sounds={sounds}
+          play={play}
+          power={power}
+          deactivateAudio={deactivateAudio}
+        />
+        <DrumControl
+          stop={stop}
+          power={power}
+          volume={volume}
+          name={soundName || soundsName[soundType]}
+          changeSoundGroup={changeSoundGroup}
+          handleVolumeChange={handleVolumeChange}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
